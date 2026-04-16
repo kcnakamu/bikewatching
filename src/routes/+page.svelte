3ee0abffeb4ac19fed8d8c230a2880739328ebfa
Karen Nakamura <kcnakamu@mit.edu>
2026-04-16 12:33:42 -0400

    Final code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,13 @@
 
 	let map;
 	let stations = [];
-	let trips;
+	let trips = [];
 	let departures;
 	let arrivals;
+
+	let stationFlow = d3.scaleQuantize()
+		.domain([0, 1])
+		.range([0, 0.5, 1]);
 
 	async function initMap() {
 		// We assign a value to the `map` variable here
@@ -58,8 +62,13 @@
 
 	async function getStations() {
 		stations = await d3.csv("https://vis-society.github.io/labs/9/data/bluebikes-stations.csv")
-		trips = await d3.csv("https://vis-society.github.io/labs/9/data/bluebikes-traffic-2024-03.csv")
-				// console.log(trips)
+		trips = await d3.csv("https://vis-society.github.io/labs/9/data/bluebikes-traffic-2024-03.csv").then(trips => {
+			for (let trip of trips) {
+				trip.started_at = new Date(trip.started_at);
+				trip.ended_at = new Date(trip.ended_at);
+			}
+			return trips;
+		});
 		departures = d3.rollup(trips, v => v.length, d => d.start_station_id);
 		arrivals = d3.rollup(trips, v => v.length, d => d.end_station_id);
 		let totalTraffic = new Map();
@@ -90,6 +99,32 @@
 		return {cx: x, cy: y};
 	}
 
+	function minutesSinceMidnight(date) {
+		return date.getHours() * 60 + date.getMinutes();
+	}
+
+	$: filteredTrips = timeFilter === -1? trips : trips.filter(trip => {
+		let startedMinutes = minutesSinceMidnight(trip.started_at);
+		let endedMinutes = minutesSinceMidnight(trip.ended_at);
+		return Math.abs(startedMinutes - timeFilter) <= 60
+			|| Math.abs(endedMinutes - timeFilter) <= 60;
+	});
+
+	$: filteredDepartures = d3.rollup(filteredTrips, v => v.length, d => d.start_station_id);
+	$: filteredArrivals = d3.rollup(filteredTrips, v => v.length, d => d.end_station_id);	
+
+	$: filteredStations = stations.map(station => {
+		const id = station.Number;
+		const arr = filteredArrivals.get(id) ?? 0;
+		const dep = filteredDepartures.get(id) ?? 0;
+		return {
+			...station,
+			arrivals: arr,
+			departures: dep,
+			totalTraffic: arr + dep
+		};
+	});
+
 
 	onMount(() => {
 		initMap();
@@ -97,15 +132,66 @@
 	})
 
 	let mapViewChanged = 0;
+	
 	$: map?.on("move", evt => mapViewChanged++);
 
 	$: radiusScale = d3.scaleSqrt()
-		.domain([0, d3.max(stations, d => d.totalTraffic) || 0])
-		.range([0, 25]);
+		.domain([0, d3.max(filteredStations, d => d.totalTraffic) || 0])
+   		.range(Number(timeFilter) === -1 ? [0, 25] : [3, 30]);
 
 	let timeFilter = -1;
 	$: timeFilterLabel = new Date(0, 0, 0, 0, timeFilter)
 					.toLocaleString("en", {timeStyle: "short"});
+	
+	const urlBase = 'https://api.mapbox.com/isochrone/v1/mapbox/';
+	const profile = 'cycling';
+	const minutes = [5, 10, 15, 20];
+	const contourColors = [
+		"03045e",
+		"0077b6",
+		"00b4d8",
+		"90e0ef"
+	]
+	let isochrone = null;
+
+	async function getIso(lon, lat) {
+		const base = `${urlBase}${profile}/${lon},${lat}`;
+		const params = new URLSearchParams({
+			contours_minutes: minutes.join(','),
+			contours_colors: contourColors.join(','),
+			polygons: 'true',
+			access_token: mapboxgl.accessToken
+		});
+		const url = `${base}?${params.toString()}`;
+
+		const query = await fetch(url, { method: 'GET' });
+		isochrone = await query.json();
+	}
+
+	let selectedStation = null;
+
+	function geoJSONPolygonToPath(feature) {
+    const path = d3.path();
+    const rings = feature.geometry.coordinates;
+
+    for (const ring of rings) {
+			for (let i = 0; i < ring.length; i++) {
+				const [lng, lat] = ring[i];
+				const { x, y } = map.project([lng, lat]);
+				if (i === 0) path.moveTo(x, y);
+				else path.lineTo(x, y);
+			}
+			path.closePath();
+		}
+		return path.toString();
+	}
+
+
+	$: if (selectedStation) {
+		getIso(+selectedStation.Long, +selectedStation.Lat);
+	} else {
+		isochrone = null;
+	}
 
 </script>
 
@@ -130,13 +216,41 @@
 <div id="map">
     <svg>
 		{#key mapViewChanged}
-			{#each stations as station}
-				<circle { ...getCoords(station) } r={radiusScale(station.totalTraffic)} fill="steelblue" />
+			{#if isochrone}
+				{#each isochrone.features as feature}
+					<path
+							d={geoJSONPolygonToPath(feature)}
+							fill={feature.properties.fillColor}
+							fill-opacity="0.2"
+							stroke="#000000"
+							stroke-opacity="0.5"
+							stroke-width="1"
+					>
+						<title>{feature.properties.contour} minutes of biking</title>
+					</path>
+				{/each}
+			{/if}
+			{#each filteredStations as station}
+				<circle { ...getCoords(station) } 
+					r={radiusScale(station.totalTraffic)} 
+					style="--departure-ratio: {stationFlow(
+						station.totalTraffic ? station.departures / station.totalTraffic : 0.5
+					)}"
+					class={station?.Number === selectedStation?.Number ? "selected" : ""}
+    					on:mousedown={() => selectedStation = selectedStation?.Number !== station?.Number ? station : null}
+					/>
 			{/each}
 		{/key}
 		
 	</svg>
 </div>
+
+<div class="legend">
+    <div style="--departure-ratio: 1">More departures</div>
+    <div style="--departure-ratio: 0.5">Balanced</div>
+    <div style="--departure-ratio: 0">More arrivals</div>
+</div>
+
 
 
 
@@ -159,6 +273,28 @@
 		pointer-events: none;
 	}
 
+	#map svg circle {
+		--color-departures: steelblue;
+		--color-arrivals: darkorange;
+		--color: color-mix(
+			in oklch,
+			var(--color-departures) calc(100% * var(--departure-ratio)),
+			var(--color-arrivals)
+		);
+		fill: var(--color);
+		transition: opacity 0.2s ease;
+		pointer-events: auto;
+	}
+
+
+	#map svg:has(circle.selected) circle:not(.selected) {
+		opacity: 0.3;
+	}
+
+	#map svg path {
+		pointer-events: auto;
+	}
+
 	#map circle {
 		background-color: steelblue;
 		fill-opacity: 60%;
@@ -175,4 +311,39 @@
 	label {
 		margin-left: auto;
 	}
+
+
+	.legend {
+		display: flex;
+		gap: 1px;
+		margin-block: 1em;
+	}
+
+	.legend > div {
+		--color-departures: steelblue;
+		--color-arrivals: darkorange;
+		--color: color-mix(
+			in oklch,
+			var(--color-departures) calc(100% * var(--departure-ratio)),
+			var(--color-arrivals)
+		);
+		flex: 1;
+		background: var(--color);
+		padding: 0.5em 1.5em;
+		color: white;
+		font-size: 0.9rem;
+	}
+
+	.legend > div:first-child {
+		text-align: left;
+	}
+
+	.legend > div:nth-child(2) {
+		text-align: center;
+	}
+
+	.legend > div:last-child {
+		text-align: right;
+	}
+	
 </style>
